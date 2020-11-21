8cc16c445b22415fa9143ef90b1a3e0e5d391eec
Kradon <kradonzhao@gmail.com>
2020-11-21 16:23:43 -0500

    go to survey start screen
    Driver will find the complete survey button and click it.
    If the user does not have a correct username or password, then it will return an error.

--- AutoSurvey.py
+++ AutoSurvey.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import config
+import time
 
 def login(driver):
     driver.get("http://patientconnect.bu.edu")
@@ -9,6 +10,11 @@ def login(driver):
     element = driver.find_element_by_id("j_password")
     element.send_keys(config.password)
     element.send_keys(Keys.ENTER)
+    time.sleep(3)
+    if "Home" not in driver.title:
+        raise Exception
+    driver.find_element_by_partial_link_text("Complete Survey").click()
+
 
 driver = webdriver.Firefox()
 login(driver)
