e3d164b8ee8c153eee1111e5a30818e7deaea528
Kradon <kradonzhao@gmail.com>
2020-11-21 16:13:27 -0500

    feat: log user into patient connect

--- AutoSurvey.py
+++ AutoSurvey.py
@@ -1,10 +1,15 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+import config
 
 def login(driver):
-    driver.get("patientconnect.bu.edu")
+    driver.get("http://patientconnect.bu.edu")
     element = driver.find_element_by_id("j_username")
     element.send_keys(config.username)
     element = driver.find_element_by_id("j_password")
     element.send_keys(config.password)
     element.send_keys(Keys.ENTER)
+
+driver = webdriver.Firefox()
+login(driver)
+driver.close()
